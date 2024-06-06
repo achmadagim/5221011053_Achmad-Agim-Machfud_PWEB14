90561e6db68c3eb58c6148b589ef3b4a5b80cd1f
achmadagim <achmadagim4@gmail.com>
2024-06-06 14:03:10 +0700

    first commit

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Universitas Kayangan Yogyakarta</title>
+    <title>Universitas Kayangan</title>
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
     <!-- Bootstrap CSS -->
     <link href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
